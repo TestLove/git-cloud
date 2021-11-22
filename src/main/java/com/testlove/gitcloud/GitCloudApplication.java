3ba8c90582862bf6e77cc279a88b068b8b6321c6
testlove <1923161393@qq.com>
2021-11-22 15:50:50 +0800

    first commit

---
+++ src/main/java/com/testlove/gitcloud/GitCloudApplication.java
@@ -0,0 +1,13 @@
+package com.testlove.gitcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitCloudApplication.class, args);
+	}
+
+}
