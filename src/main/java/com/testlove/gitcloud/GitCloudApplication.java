cf43183bb8eda4d6ca7f9bd189600d7b2744aaaa
Totora6 <2213825668@qq.com>
2021-11-23 22:35:52 +0800

    hello

--- src/main/java/com/testlove/gitcloud/GitCloudApplication.java
+++ src/main/java/com/testlove/gitcloud/GitCloudApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GitCloudApplication {
 
 	public static void main(String[] args) {
+		System.out.println("hello Totora6!");
 		SpringApplication.run(GitCloudApplication.class, args);
 	}
 
