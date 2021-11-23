ec08c4150a6fdbe2c97404c12ca0d7e87526eed5
testlove <1923161393@qq.com>
2021-11-23 15:41:08 +0800

    创建老师类

---
+++ src/main/java/com/testlove/gitcloud/entity/Teacher.java
@@ -0,0 +1,11 @@
+package com.testlove.gitcloud.entity;
+
+/**
+ * @author TestLove
+ * @version 1.0
+ * @date 2021/11/23 15:40
+ * @Description: null
+ */
+public class Teacher {
+    String name;
+}
