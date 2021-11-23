df4aa94f47e81602516da5f2dd1f15bfa403fb0e
wxb6666 <85444711+wxb6666@users.noreply.github.com>
2021-11-23 18:17:00 +0800

    zqq第一次修改代码

--- src/main/java/com/testlove/gitcloud/service/WxbAndZqqService.java
+++ src/main/java/com/testlove/gitcloud/service/WxbAndZqqService.java
@@ -3,5 +3,6 @@ package com.testlove.gitcloud.service;
 public class WxbAndZqqService {
     public static void main(String[] args) {
         System.out.println("wxb开始写业务代码");
+        System.out.println("zqq第一次修改代码");
     }
 }
