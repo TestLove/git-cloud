f0215837542fc0084f46f215b402707e66b4e5b1
testlove <1923161393@qq.com>
2021-11-23 09:38:35 +0800

    创建学生类

---
+++ src/main/java/com/testlove/gitcloud/entity/Student.java
@@ -0,0 +1,11 @@
+package com.testlove.gitcloud.entity;
+
+/**
+ * @author TestLove
+ * @version 1.0
+ * @date 2021/11/23 9:36
+ * @Description: null
+ */
+public class Student {
+    String name;
+}
